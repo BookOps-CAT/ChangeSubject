deb60efe78b142beeb6dfba4c09972aac3b5b013
klinga <klingaroo@gmail.com>
2021-12-09 23:01:20 -0500

    adds adopt_lc()

--- src/global_update.py
+++ src/global_update.py
@@ -1,21 +1,22 @@
 """
-This scripts globally updates BPL & NYPL records to convert local Change the Subject headings to
-LC terms or older local terms to new local terms.
-In particular this script handles split of "undocumented" headings into two "Noncitizens" and "Undocumened immigration"
+This scripts globally updates BPL & NYPL records to convert local 
+Change the Subject headings to LC terms or older local terms to new local terms.
+In particular this script handles split of "undocumented" headings into two 
+"Noncitizens" and "Undocumened immigration"
 """
 import sys
 
-from pymarc import MARCReader
+from pymarc import MARCReader, Field, Record
 
 
-from .december_changes import ADOPTED
+from src.december_changes import ADOPTED
 
 
 def change_term_source_coding(field):
     pass
 
 
-def is_bookops_term(field):
+def is_bookops_term(field: Field) -> bool:
     try:
         if field["2"] == "bookops":
             return True
@@ -25,21 +26,42 @@ def is_bookops_term(field):
         return False
 
 
-def update_adopted_lc(bib):
+def adopt_lc(bib: Record) -> None:
+    """
+    Changes local bookops subject fields into LC equivalent.
+    Changes indicators coding and removes $2 with bookops
+    """
+    bibNo = bib["907"]["a"][1:]
     for term in ADOPTED:
         subjects = bib.subjects()
         for subject in subjects:
             # print(type(subject))
-            if is_bookops_term(subject) and term[0] in subject.value():
-                print(f"{term[0]} found: {subject}")
+            if is_bookops_term(subject) and term in subject.value():
+                print(f"Adopting LC term in field: {subject} ({bibNo})")
+                bib.add_ordered_field(
+                    Field(
+                        tag=subject.tag,
+                        indicators=[" ", "0"],
+                        subfields=subject.subfields[:-2],
+                    )
+                )
+                bib.remove_field(subject)
+
+
+def save2marc(fh: str, record: Record) -> None:
+    with open(fh, "ab") as marcfile:
+        marcfile.write(record.as_marc())
 
 
 def update(file_path: str):
     print(f"Processing file: {file_path}")
+    out = f"{file_path[:-4]}-PROC.mrc"
     with open(file_path, "rb") as marcfile:
         reader = MARCReader(marcfile)
         for bib in reader:
-            update_adopted_lc(bib)
+            adopt_lc(bib)
+
+            save2marc(out, bib)
 
 
 if __name__ == "__main__":
--- tests/test_global_update.py
+++ tests/test_global_update.py
@@ -1,7 +1,7 @@
-from pymarc import Field
+from pymarc import Field, Record
 import pytest
 
-from src.global_update import is_bookops_term
+from src.global_update import adopt_lc, is_bookops_term
 
 
 @pytest.mark.parametrize(
@@ -15,3 +15,29 @@ from src.global_update import is_bookops_term
 def test_is_bookops_term(arg, expectation):
     field = Field(tag="650", subfields=arg)
     assert is_bookops_term(field) == expectation
+
+
+def test_adopt_lc(stub_bib):
+    stub_bib.add_field(
+        Field(
+            tag="650",
+            indicators=[" ", "7"],
+            subfields=[
+                "a",
+                "Noncitizen criminals",
+                "z",
+                "United States.",
+                "2",
+                "bookops",
+            ],
+        )
+    )
+    adopt_lc(stub_bib)
+    fields = stub_bib.subjects()
+    assert len(fields) == 4
+    for field in fields:
+        assert "bookops" not in field.subfields
+
+    changed = fields[-1]
+    assert changed.indicators == [" ", "0"]
+    assert changed.subfields == ["a", "Noncitizen criminals", "z", "United States."]
