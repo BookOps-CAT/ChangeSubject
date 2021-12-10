fff06eefbbea92867857a48856d1993d96ba08e7
Tomek <klingaroo@gmail.com>
2021-12-10 14:37:11 -0500

    completes processing

--- src/global_update.py
+++ src/global_update.py
@@ -9,7 +9,7 @@ import sys
 from pymarc import MARCReader, Field, Record
 
 
-from src.december_changes import ADOPTED
+from december_changes import ADOPTED, SPLIT, CHANGED
 
 
 def change_term_source_coding(field):
@@ -48,6 +48,79 @@ def adopt_lc(bib: Record) -> None:
                 bib.remove_field(subject)
 
 
+def normalize_subfield(value):
+    return value.replace(".", "")
+
+
+def isolate_tail(field: Field) -> list:
+    """
+    Returns subfields following the main $a term
+    """
+    return field.subfields[2:-2]
+
+
+def split_local(bib: Record) -> None:
+    """
+    Flips local terms that have been changed.
+    """
+    bibNo = bib["907"]["a"][1:]
+    for old_term, new_term in SPLIT:
+        subjects = bib.subjects()
+        for subject in subjects:
+            if is_bookops_term(subject) and old_term in subject["a"]:
+                print(f"Spliting field {subject} ({bibNo})")
+                tail = isolate_tail(subject)
+                if not tail:
+                    main_subs = ["a", f"{new_term}."]
+                else:
+                    main_subs = ["a", new_term]
+                    main_subs.extend(tail)
+                bib.add_ordered_field(
+                    Field(
+                        tag=subject.tag,
+                        indicators=[" ", "0"],
+                        subfields=main_subs,
+                    )
+                )
+                if not tail:
+                    extra_subs = ["a", "Undocumented immigration."]
+                else:
+                    extra_subs = ["a", "Undocumented immigration"]
+                    extra_subs.extend(tail)
+                extra_subs.extend(["2", "bookops"])
+                bib.add_ordered_field(
+                    Field(
+                        tag="650",
+                        indicators=[" ", "7"],
+                        subfields=extra_subs,
+                    )
+                )
+                bib.remove_field(subject)
+
+
+def change_local(bib: Record) -> None:
+    """
+    Makes one to one local change
+    """
+    bibNo = bib["907"]["a"][1:]
+    for old_term, new_term in CHANGED:
+        subjects = bib.subjects()
+        for subject in subjects:
+            if is_bookops_term(subject) and old_term in subject["a"]:
+                print(f"Changing local {subject} ({bibNo})")
+                tail = isolate_tail(subject)
+                if not tail:
+                    subs = ["a", f"{new_term}.", "2", "bookops"]
+                else:
+                    subs = ["a", new_term]
+                    subs.extend(tail)
+                    subs.extend(["2", "bookops"])
+                bib.add_ordered_field(
+                    Field(tag=subject.tag, indicators=[" ", "7"], subfields=subs)
+                )
+                bib.remove_field(subject)
+
+
 def save2marc(fh: str, record: Record) -> None:
     with open(fh, "ab") as marcfile:
         marcfile.write(record.as_marc())
@@ -60,6 +133,8 @@ def update(file_path: str):
         reader = MARCReader(marcfile)
         for bib in reader:
             adopt_lc(bib)
+            split_local(bib)
+            change_local(bib)
 
             save2marc(out, bib)
 
--- tests/test_global_update.py
+++ tests/test_global_update.py
@@ -1,7 +1,14 @@
 from pymarc import Field, Record
 import pytest
 
-from src.global_update import adopt_lc, is_bookops_term
+from src.global_update import (
+    adopt_lc,
+    change_local,
+    is_bookops_term,
+    isolate_tail,
+    normalize_subfield,
+    split_local,
+)
 
 
 @pytest.mark.parametrize(
@@ -17,27 +24,154 @@ def test_is_bookops_term(arg, expectation):
     assert is_bookops_term(field) == expectation
 
 
-def test_adopt_lc(stub_bib):
+@pytest.mark.parametrize("arg,expectation", [("Foo", "Foo"), ("Foo.", "Foo")])
+def test_normalize_subfield(arg, expectation):
+    assert normalize_subfield(arg) == expectation
+
+
+def test_isolate_tail():
+    field = Field(
+        tag="650",
+        indicators=[" ", "7"],
+        subfields=["a", "Foo", "x", "Bar.", "2", "bookops"],
+    )
+    assert isolate_tail(field) == ["x", "Bar."]
+
+
+@pytest.mark.parametrize(
+    "arg",
+    [
+        ["a", "Noncitizens."],
+        ["a", "Noncitizen criminals."],
+        ["a", "Noncitizens (Greek law)."],
+        ["a", "Noncitizens in art."],
+        ["a", "Noncitizens", "x", "Care", "z", "United States."],
+        ["a", "Church work with noncitizens."],
+        ["a", "Corporations, Foreign."],
+    ],
+)
+def test_adopt_lc(stub_bib, arg):
+    subfields = arg[:]
+    subfields.extend(["2", "bookops"])
     stub_bib.add_field(
         Field(
             tag="650",
             indicators=[" ", "7"],
-            subfields=[
-                "a",
-                "Noncitizen criminals",
-                "z",
-                "United States.",
-                "2",
-                "bookops",
-            ],
+            subfields=subfields,
         )
     )
     adopt_lc(stub_bib)
     fields = stub_bib.subjects()
     assert len(fields) == 4
-    for field in fields:
-        assert "bookops" not in field.subfields
-
     changed = fields[-1]
     assert changed.indicators == [" ", "0"]
-    assert changed.subfields == ["a", "Noncitizen criminals", "z", "United States."]
+    assert changed.subfields == subfields[:-2]
+
+
+@pytest.mark.parametrize(
+    "arg,expectation1,expectation2",
+    [
+        (
+            ["a", "Children of undocumented immigrants.", "2", "bookops"],
+            ["a", "Children of noncitizens."],
+            ["a", "Undocumented immigration.", "2", "bookops"],
+        ),
+        (
+            [
+                "a",
+                "Undocumented children",
+                "x",
+                "Government policy",
+                "z",
+                "United States.",
+                "2",
+                "bookops",
+            ],
+            [
+                "a",
+                "Noncitizen children",
+                "x",
+                "Government policy",
+                "z",
+                "United States.",
+            ],
+            [
+                "a",
+                "Undocumented immigration",
+                "x",
+                "Government policy",
+                "z",
+                "United States.",
+                "2",
+                "bookops",
+            ],
+        ),
+        (
+            ["a", "Undocumented immigrants", "z", "United States.", "2", "bookops"],
+            ["a", "Noncitizens", "z", "United States."],
+            ["a", "Undocumented immigration", "z", "United States.", "2", "bookops"],
+        ),
+        (
+            ["a", "Women undocumented immigrants.", "2", "bookops"],
+            ["a", "Women noncitizens."],
+            ["a", "Undocumented immigration.", "2", "bookops"],
+        ),
+    ],
+)
+def test_split_local(stub_bib, arg, expectation1, expectation2):
+    subfields = arg[:]
+    stub_bib.add_field(Field(tag="650", indicators=[" ", "7"], subfields=subfields))
+    # print(stub_bib)
+    assert len(stub_bib.subjects()) == 4
+    split_local(stub_bib)
+
+    fields = stub_bib.subjects()
+    assert len(fields) == 5
+    changedA = fields[-2]
+    changedB = fields[-1]
+
+    assert changedA.indicators == [" ", "0"]
+    assert changedA.subfields == expectation1
+
+    assert changedB.indicators == [" ", "7"]
+    assert changedB.subfields == expectation2
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        (
+            ["a", "Undocumented immigrants in literature.", "2", "bookops"],
+            ["a", "Undocumented immigration in literature.", "2", "bookops"],
+        ),
+        (
+            [
+                "a",
+                "Undocumented immigrants in literature",
+                "v",
+                "Juvenile literature.",
+                "2",
+                "bookops",
+            ],
+            [
+                "a",
+                "Undocumented immigration in literature",
+                "v",
+                "Juvenile literature.",
+                "2",
+                "bookops",
+            ],
+        ),
+    ],
+)
+def test_change_local(stub_bib, arg, expectation):
+    stub_bib.add_field(Field(tag="650", indicators=[" ", "7"], subfields=arg))
+    assert len(stub_bib.subjects()) == 4
+
+    change_local(stub_bib)
+
+    fields = stub_bib.subjects()
+    assert len(fields) == 4
+    changed = fields[-1]
+    assert changed.indicators == [" ", "7"]
+    assert changed.subfields == expectation
